f8e62a66faef9276d968307efa657a4a954025e8
Oliver Uvman <oliver.uvman@gmail.com>
2013-07-10 16:37:15 +0200

    fixed local_settings.py ignore

--- screencast/local_settings_template.py
+++ screencast/local_settings_template.py
@@ -1,7 +1,14 @@
+
+#  |  Settings template
+#  |
+#  |  Copy this file to local_settings.py in the same directory,
+#  |  and change the settings so that they agree with your setup.
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',     # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/albin/Dropbox/ida/env/screencast/sq3.db',  # Or path to database file if using sqlite3.
+        'NAME': '/home/oliver/.screencast-sq3.db',  # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -14,12 +21,12 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/albin/Dropbox/ida/env/screencast/static',
+    '/home/oliver/Projects/screencast/static',
 )
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/albin/Dropbox/ida/env/screencast/templates',
+    '/home/oliver/Projects/screencast/templates',
 )
