164cc8f7c42829dc99acfa6a600e8269e88a763d
Oliver Uvman <oliver.uvman@gmail.com>
2013-07-19 20:20:35 +0200

    wip: autocomplete almost done! :D

--- static/js/create_group.coffee
+++ static/js/create_group.coffee
@@ -5,36 +5,27 @@ $ ->
   dom_selected_group_column = $('#groupcolumn')
   dom_selected_group_list = $('#selected_group')
   dom_selected_group_name = $('#group_name')
-  selected_users = []
+  dom_create_button = $('#create_group_button')
+  dom_group_name_box = $('#groupnameinput')
+  dom_result = $('#result')
+
+  currently_selected_users = []
+  past_selected_users = []
   selected_group = ''
   selected_group_members = []
-
-
-  filter_selected_users = (user_group_list) ->
-    user_group_list.filter((item) ->
-      if item.category == '' and item.value in selected_users
-        return false
-      return true)
-
-
-  refresh_gui = () ->
-    dom_selected_users_list.empty()
-    for username in selected_users
-      dom_selected_users_list.append($("<li class='added_user'>#{username}</li>"))
-    dom_selected_group_list.empty()
-
-    if selected_group == ''
-      dom_selected_group_column.hide()
-    else
-      dom_selected_group_name.text(selected_group)
-      for username in selected_group_members
-        dom_selected_group_list.append(create_username_box(username))
-      dom_selected_group_column.show()
+  cache = {}
 
 
   select_user = (username) ->
-    return if username in selected_users
-    selected_users.push(username)
+    return if username in currently_selected_users
+    currently_selected_users.push(username)
+    past_selected_users.push(username)
+    refresh_gui()
+
+
+  deselect_user = (username) ->
+    return if username not in currently_selected_users
+    currently_selected_users.remove(username)
     refresh_gui()
 
 
@@ -45,40 +36,91 @@ $ ->
     refresh_gui()
 
 
-  create_username_box = (username) ->
-    box_class = "non_added_user"
-    box_class = "added_user" if username in selected_users
-    return $("<li class='#{box_class}'><a>#{username}</a></li>")
+  refresh_gui = ->
+    dom_selected_users_list.empty()
+    dom_selected_group_list.empty()
+
+    users_to_show = _.union(currently_selected_users, past_selected_users)
+    users_to_show = _.sortBy users_to_show, (uname) ->
+      return uname.toUpperCase()
+
+    if users_to_show == []
+      dom_selected_user_column.hide()
+    else
+      dom_selected_user_column.show()
+      for username in users_to_show
+        dom_selected_users_list.append(create_username_clickable_box(username))
+
+    if selected_group == ''
+      dom_selected_group_column.hide()
+    else
+      dom_selected_group_name.text(selected_group)
+      for username in selected_group_members
+        dom_selected_group_list.append(create_username_clickable_box(username))
+      dom_selected_group_column.show()
 
 
-  create_group_box = (group_name, group_members) ->
+  get_username_selection_class = (username) ->
+    if username in currently_selected_users
+      return "added_user"
+    else
+      return "non_added_user"
+
+
+  get_group_selection_class = (group_name, group_members) ->
     all_members_selected = true
     any_member_selected = false
     for username in group_members
-      if username in selected_users
+      if username in currently_selected_users
         any_member_selected = true
       else
         all_members_selected = false
 
     if all_members_selected
-      return $("<li class='all_selected_group'><a>#{group_name}</a></li>")
+      return 'all_selected_group'
     else if any_member_selected
-      return $("<li class='some_selected_group'><a>#{group_name}</a></li>")
+      return 'some_selected_group'
     else
-      return $("<li class='none_selected_group'><a>#{group_name}</a></li>")
+      return 'none_selected_group'
+
+
+  create_username_complete_box = (username) ->
+    return $("<li class='#{get_username_selection_class(username)}'><a>#{username}</a></li>")
+
+
+  create_group_complete_box = (group_name, group_members) ->
+    css_selection_class = get_group_selection_class(group_name, group_members)
+    return $("<li class='#{css_selection_class}'><a>#{group_name}</a></li>")
+
+
+  create_username_clickable_box = (username) ->
+    box = $("<li class='#{get_username_selection_class(username)}'>#{username}</li>")
+    box.on 'click', ->
+      if username in currently_selected_users
+        deselect_user(username)
+      else
+        select_user(username)
 
 
   create_category_list_item = (category_name) ->
     return $("<li class='ui-autocomplete-category'>#{category_name}</li>")
 
 
+  # Used to filter out already selected users from the autocomplete suggestions
+  filter_selected_users = (user_group_list) ->
+    _.filter user_group_list, (item) ->
+      if item.category == '' and item.value in currently_selected_users
+        return false
+      return true
+
+
   # Subclass autocomplete widget to enable custom drawing and categories.
-  $.widget("custom.user_group_complete", $.ui.autocomplete, {
+  $.widget "custom.user_group_complete", $.ui.autocomplete,
     _renderItem: (ul, item) ->
       if item.category == ''
-        create_username_box(item.value).appendTo(ul)
+        create_username_complete_box(item.value).appendTo(ul)
       else
-        create_group_box(item.value, item.members).appendTo(ul)
+        create_group_complete_box(item.value, item.members).appendTo(ul)
     _renderMenu: (ul, items) ->
       that = this
       current_category = ''
@@ -88,11 +130,9 @@ $ ->
           ul.append(create_category_list_item(item.category))
           current_category = item.category
         that._renderItemData(ul, item)
-  })
 
-  cache = {}
 
-  dom_complete_box.user_group_complete({
+  dom_complete_box.user_group_complete
     source: (request, response) ->
       search_string = request.term
       if search_string of cache
@@ -113,4 +153,22 @@ $ ->
       # Return false to inhibit insertion of the selected value
       # into the input field.
       return false
-  })
+
+
+  dom_create_button.on "click", ->
+    group_name = dom_group_name_box.val()
+    if group_name?.length
+      # TODO warn about empty group name
+      null
+
+    $.ajax
+      url: 'create_group/'
+      type: 'POST'
+      dataType: 'JSON'
+      data:
+        groupname: group_name
+        users: currently_selected_users
+      success: (result) ->
+        dom_result.text(result)  # TODO report things beautifully
+      error: (xhr, errmsg, err) ->
+        dom_result.text('ERROR: ' + xhr + errmsg + err)  # TODO report things beautifully
--- static/js/create_group.js
+++ static/js/create_group.js
@@ -3,49 +3,34 @@
   var __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
   $(function() {
-    var cache, create_category_list_item, create_group_box, create_username_box, dom_complete_box, dom_selected_group_column, dom_selected_group_list, dom_selected_group_name, dom_selected_user_column, dom_selected_users_list, filter_selected_users, refresh_gui, select_group, select_user, selected_group, selected_group_members, selected_users;
+    var cache, create_category_list_item, create_group_complete_box, create_username_clickable_box, create_username_complete_box, currently_selected_users, deselect_user, dom_complete_box, dom_create_button, dom_group_name_box, dom_result, dom_selected_group_column, dom_selected_group_list, dom_selected_group_name, dom_selected_user_column, dom_selected_users_list, filter_selected_users, get_group_selection_class, get_username_selection_class, past_selected_users, refresh_gui, select_group, select_user, selected_group, selected_group_members;
     dom_complete_box = $("#username_input");
     dom_selected_users_list = $('#selected_users');
     dom_selected_user_column = $('#usercolumn');
     dom_selected_group_column = $('#groupcolumn');
     dom_selected_group_list = $('#selected_group');
     dom_selected_group_name = $('#group_name');
-    selected_users = [];
+    dom_create_button = $('#create_group_button');
+    dom_group_name_box = $('#groupnameinput');
+    dom_result = $('#result');
+    currently_selected_users = [];
+    past_selected_users = [];
     selected_group = '';
     selected_group_members = [];
-    filter_selected_users = function(user_group_list) {
-      return user_group_list.filter(function(item) {
-        var _ref;
-        if (item.category === '' && (_ref = item.value, __indexOf.call(selected_users, _ref) >= 0)) {
-          return false;
-        }
-        return true;
-      });
-    };
-    refresh_gui = function() {
-      var username, _i, _j, _len, _len1;
-      dom_selected_users_list.empty();
-      for (_i = 0, _len = selected_users.length; _i < _len; _i++) {
-        username = selected_users[_i];
-        dom_selected_users_list.append($("<li class='added_user'>" + username + "</li>"));
-      }
-      dom_selected_group_list.empty();
-      if (selected_group === '') {
-        return dom_selected_group_column.hide();
-      } else {
-        dom_selected_group_name.text(selected_group);
-        for (_j = 0, _len1 = selected_group_members.length; _j < _len1; _j++) {
-          username = selected_group_members[_j];
-          dom_selected_group_list.append(create_username_box(username));
-        }
-        return dom_selected_group_column.show();
-      }
-    };
+    cache = {};
     select_user = function(username) {
-      if (__indexOf.call(selected_users, username) >= 0) {
+      if (__indexOf.call(currently_selected_users, username) >= 0) {
         return;
       }
-      selected_users.push(username);
+      currently_selected_users.push(username);
+      past_selected_users.push(username);
+      return refresh_gui();
+    };
+    deselect_user = function(username) {
+      if (__indexOf.call(currently_selected_users, username) < 0) {
+        return;
+      }
+      currently_selected_users.remove(username);
       return refresh_gui();
     };
     select_group = function(group_name, group_members) {
@@ -56,43 +41,98 @@
       selected_group_members = group_members;
       return refresh_gui();
     };
-    create_username_box = function(username) {
-      var box_class;
-      box_class = "non_added_user";
-      if (__indexOf.call(selected_users, username) >= 0) {
-        box_class = "added_user";
+    refresh_gui = function() {
+      var username, users_to_show, _i, _j, _len, _len1;
+      dom_selected_users_list.empty();
+      dom_selected_group_list.empty();
+      users_to_show = _.union(currently_selected_users, past_selected_users);
+      users_to_show = _.sortBy(users_to_show, function(uname) {
+        return uname.toUpperCase();
+      });
+      if (users_to_show === []) {
+        dom_selected_user_column.hide();
+      } else {
+        dom_selected_user_column.show();
+        for (_i = 0, _len = users_to_show.length; _i < _len; _i++) {
+          username = users_to_show[_i];
+          dom_selected_users_list.append(create_username_clickable_box(username));
+        }
       }
-      return $("<li class='" + box_class + "'><a>" + username + "</a></li>");
+      if (selected_group === '') {
+        return dom_selected_group_column.hide();
+      } else {
+        dom_selected_group_name.text(selected_group);
+        for (_j = 0, _len1 = selected_group_members.length; _j < _len1; _j++) {
+          username = selected_group_members[_j];
+          dom_selected_group_list.append(create_username_clickable_box(username));
+        }
+        return dom_selected_group_column.show();
+      }
     };
-    create_group_box = function(group_name, group_members) {
+    get_username_selection_class = function(username) {
+      if (__indexOf.call(currently_selected_users, username) >= 0) {
+        return "added_user";
+      } else {
+        return "non_added_user";
+      }
+    };
+    get_group_selection_class = function(group_name, group_members) {
       var all_members_selected, any_member_selected, username, _i, _len;
       all_members_selected = true;
       any_member_selected = false;
       for (_i = 0, _len = group_members.length; _i < _len; _i++) {
         username = group_members[_i];
-        if (__indexOf.call(selected_users, username) >= 0) {
+        if (__indexOf.call(currently_selected_users, username) >= 0) {
           any_member_selected = true;
         } else {
           all_members_selected = false;
         }
       }
       if (all_members_selected) {
-        return $("<li class='all_selected_group'><a>" + group_name + "</a></li>");
+        return 'all_selected_group';
       } else if (any_member_selected) {
-        return $("<li class='some_selected_group'><a>" + group_name + "</a></li>");
+        return 'some_selected_group';
       } else {
-        return $("<li class='none_selected_group'><a>" + group_name + "</a></li>");
+        return 'none_selected_group';
       }
+    };
+    create_username_complete_box = function(username) {
+      return $("<li class='" + (get_username_selection_class(username)) + "'><a>" + username + "</a></li>");
+    };
+    create_group_complete_box = function(group_name, group_members) {
+      var css_selection_class;
+      css_selection_class = get_group_selection_class(group_name, group_members);
+      return $("<li class='" + css_selection_class + "'><a>" + group_name + "</a></li>");
+    };
+    create_username_clickable_box = function(username) {
+      var box;
+      box = $("<li class='" + (get_username_selection_class(username)) + "'>" + username + "</li>");
+      return box.on('click', function() {
+        if (__indexOf.call(currently_selected_users, username) >= 0) {
+          return deselect_user(username);
+        } else {
+          return select_user(username);
+        }
+      });
     };
     create_category_list_item = function(category_name) {
       return $("<li class='ui-autocomplete-category'>" + category_name + "</li>");
     };
+    filter_selected_users = function(user_group_list) {
+      return _.filter(user_group_list, function(item) {
+        var _ref;
+        if (item.category === '' && (_ref = item.value, __indexOf.call(currently_selected_users, _ref) >= 0)) {
+          return false;
+        }
+        return true;
+      });
+    };
     $.widget("custom.user_group_complete", $.ui.autocomplete, {
       _renderItem: function(ul, item) {
         if (item.category === '') {
-          return create_username_box(item.value).appendTo(ul);
+          return create_username_complete_box(item.value).appendTo(ul);
         } else {
-          return create_group_box(item.value, item.members).appendTo(ul);
+          return create_group_complete_box(item.value, item.members).appendTo(ul);
         }
       },
       _renderMenu: function(ul, items) {
@@ -111,8 +151,7 @@
         return _results;
       }
     });
-    cache = {};
-    return dom_complete_box.user_group_complete({
+    dom_complete_box.user_group_complete({
       source: function(request, response) {
         var search_string;
         search_string = request.term;
@@ -136,6 +175,28 @@
         dom_complete_box.val('');
         return false;
       }
+    });
+    return dom_create_button.on("click", function() {
+      var group_name;
+      group_name = dom_group_name_box.val();
+      if (group_name != null ? group_name.length : void 0) {
+        null;
+      }
+      return $.ajax({
+        url: 'create_group/',
+        type: 'POST',
+        dataType: 'JSON',
+        data: {
+          groupname: group_name,
+          users: currently_selected_users
+        },
+        success: function(result) {
+          return dom_result.text(result);
+        },
+        error: function(xhr, errmsg, err) {
+          return dom_result.text('ERROR: ' + xhr + errmsg + err);
+        }
+      });
     });
   });
 
